5b6d2a19d13ce4eeac4bd0cda68aba9f0e7bac37
Justina <justina.bauziene@gmail.com>
2019-05-10 18:28:52 +0300

    +

---
+++ Project/Project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Project
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double plotas = 0;
+            double turis = 0;
+            double izambine = 0;
+
+            Console.WriteLine("Kintamuju suma: {0}", plotas + turis + izambine);
+            Console.WriteLine("Kintamuju vidurkis: {0}", (plotas + turis + izambine) / 3);
+
+        }
+    }
+}
