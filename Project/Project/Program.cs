7bf6e533e5f79bd81f205b967009dda955d07da1
Donatas Noreika <donoras@gmail.com>
2019-05-10 18:43:30 +0300

    Idejau plota

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -6,12 +6,15 @@ namespace Project
     {
         public static void Main(string[] args)
         {
-            double plotas = 0;
+            double plotas = Math.PI * Math.Pow(6371, 2);
             double turis = 0;
             double izambine = 0;
 
+
             Console.WriteLine("Kintamuju suma: {0}", plotas + turis + izambine);
             Console.WriteLine("Kintamuju vidurkis: {0}", (plotas + turis + izambine) / 3);
+
+            Console.ReadLine();
 
         }
     }
