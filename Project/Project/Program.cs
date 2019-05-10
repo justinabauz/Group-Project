249b06abcd69c1be5dbad509b34b1fcb1e43970b
Donatas Noreika <donoras@gmail.com>
2019-05-10 18:46:49 +0300

    Pridejau isvedima

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -11,6 +11,10 @@ namespace Project
             double izambine = 0;
 
 
+            Console.WriteLine("Zemes rutulio plotas: {0}", plotas);
+            Console.WriteLine("Zemes rutulio turid: {0}", turis);
+            Console.WriteLine("Staciojo trikampio izambines ilgis: {0}", izambine);
+
             Console.WriteLine("Kintamuju suma: {0}", plotas + turis + izambine);
             Console.WriteLine("Kintamuju vidurkis: {0}", (plotas + turis + izambine) / 3);
 
